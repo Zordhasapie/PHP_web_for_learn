40660850944ba1dc84c2a3f7b550944a298c9bd6
Phạm Quang Sáng <sangvinhct@gmail.com>
2022-03-24 17:54:48 +0700

    add function.php

---
+++ php_handle/function.php
@@ -0,0 +1,21 @@
+<?php
+function redirect($URL)
+{
+    header('Location: ' . $URL);
+}
+
+function is_logged_in()
+{
+    return isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true;
+}
+
+function login($username, $password)
+{
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+}
+
+function logout(){
+    session_destroy();
+    redirect('/login.php');
+}
